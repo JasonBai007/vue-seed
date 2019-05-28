12061bd61bebfb992a55947f8014a67905d9dcad
JasonBai007 <byygkcg@163.com>
2019-05-28 16:56:40 +0800

    添加备用情况注释

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <template>
-    <div id="app">
-        <router-view></router-view>
-    </div>
+  <div id="app">
+    <router-view></router-view>
+    <!-- 如果不同路由渲染同一组件，会导致数据不更新，这时候可以巧妙的加一个不同的Key -->
+    <!-- <router-view :key="$route.fullpath"></router-view> -->
+  </div>
 </template>
 <script>
 export default {
-  name: 'app'
-}
+  name: "app"
+};
 </script>
 <style lang="scss">
 // 这里是坑啊，文件路径中好像不能用@符号
